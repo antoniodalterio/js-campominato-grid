5aea8a09d3049658d6275604001581dac5e0d2de
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-15 15:27:12 +0100

    feat: div.cell tramite js fase bozza

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict';
-console.log('salve a tutti');
+
+// Funzioni
+
+const board = document.querySelector('.board');
+
+for (let i = 1; i <= 100; i++) {
+  const cell = document.createElement('div');
+  cell.append(i);
+  cell.classList.add('cell');
+  console.log(cell);
+
+  board.append(cell);
+}
