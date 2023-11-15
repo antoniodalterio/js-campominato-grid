93a9f0d62fcfb1e8aa0494c1554cf6713761867a
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-15 17:15:45 +0100

    feat: evento click ad ogni cella

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ function cellFunction(tag, className, content) {
   const element = document.createElement(tag);
   element.append(content);
   element.classList.add(className);
+
+  // funzione per evento click
+  element.addEventListener('click', function () {
+    element.classList.toggle('bg-primary');
+  });
   return element;
 }
 
@@ -19,14 +24,16 @@ Funzioni
 
 const board = document.querySelector('.board');
 let cell = '';
+const btnPlay = document.getElementById('bottone-play');
 
+// Ciclo creazione elementi nel document html
 for (let i = 1; i <= 100; i++) {
   cell = cellFunction('div', 'cell', i);
   board.append(cell);
   console.log(cell);
 }
 
-const btnPlay = document.getElementById('bottone-play');
+// eventoi al click del bottone
 
 btnPlay.addEventListener('click', function (e) {
   board.classList.toggle('d-none');
